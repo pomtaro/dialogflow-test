d7479279029bf2fc12c15ad27af39028fe97a4a9
HigashiTakanori_LS <pomtaro0509@gmail.com>
2019-02-23 18:10:20 +0900

    get entity

--- dialogflow_app.py
+++ dialogflow_app.py
@@ -30,10 +30,42 @@ def webhook():
     print('***** post data *****')
     print(data)
 
+    # intentの確認
     print('intent')
     print(data['queryResult']['intent']['displayName'])
 
+    # parameterの確認、Entity
+    print('entity')
+    print(data['queryResult']['parameters']['building'])
+
+    # sender id
+    sender_id = data['originalDetectIntentRequest']['payload']['data']['sender']['id']
+
+    # send message
+    text = 'dialogflowからメッセージを受け取ったよ'
+    send_message(sender_id, text, ACCESS_TOKEN)
+
     return "ok", 200
+
+
+def send_message(recipient_id, text, access_token):
+
+    params = {
+        "access_token": access_token
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": text
+        }
+    })
+
+    requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
 
 if __name__ == '__main__':
